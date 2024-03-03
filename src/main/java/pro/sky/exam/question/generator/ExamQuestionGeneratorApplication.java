f81eea47ee14d753cc0bd5af8f7820e7e1063ed6
1XSTRY <kosogornov99@gmail.com>
2024-03-03 13:08:45 +0500

    initial commit

---
+++ src/main/java/pro/sky/exam/question/generator/ExamQuestionGeneratorApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.exam.question.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamQuestionGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamQuestionGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/exam/question/generator/ExamQuestionGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.exam.question.generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamQuestionGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
